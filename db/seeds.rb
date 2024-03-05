742df4473618ebfdfdf88a7634da85420a6d59ca
Laura Fernandez <laurafcavia@gmail.com>
2024-03-05 14:53:58 +0100

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,25 @@ User.destroy_all
 # Card.destroy_all
 
 puts "Loading avatars..."
-avatar_arr = []
+
+avatar_monkey = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_bear = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_giraffe = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_gorila = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_chicken = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_lion = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_sheep = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_tiger = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_owl = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_polar = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_lea = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_simba = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_fox = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_monkey2 = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+avatar_gorila2 = URI.open("https://collection.cloudinary.com/doind6rcn/3f635ee4be0880db3032447dfa9e27c0")
+
+avatar_arr = [avatar_monkey, avatar_bear, avatar_giraffe, avatar_gorila, avatar_chicken, avatar_lion, avatar_sheep, avatar_tiger, avatar_owl, avatar_polar, avatar_lea, avatar_simba, avatar_fox, avatar_monkey2, avatar_gorila2]
+
 
 # Game.new to nest users
 4.times do
