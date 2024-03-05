d4cdfa4ca0102b12d475818192155515c2cdcb0c
Laura Fernandez <laurafcavia@gmail.com>
2024-03-05 12:13:54 +0100

    users controller, new and create methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :photo)
+  end
+
+end
