ecc0502750abf9be75fd9a89f984353c99686167
Laura Fernandez <laurafcavia@gmail.com>
2024-03-05 14:15:17 +0100

    add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Dropping DB..."
+
+# Game.destroy_all
+User.destroy_all
+# Card.destroy_all
+
+puts "Loading avatars..."
+avatar_arr = []
+
+# Game.new to nest users
+4.times do
+  # avatar = select random img from avatar range
+
+  avatar = avatar_arr.sample
+
+  username = Faker::Name.unique.firstname
+  user = User.new(username: username)
+  user.photo.attach(io: avatar, filename: "#{username}.png", content_type: "image/png")
+
+  # 5.times do
+  #   Card.new will come here
+  # end
+end
