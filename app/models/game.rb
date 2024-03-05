81cd562051fb957397cb56942ce32d31f323945c
Laura Fernandez <laurafcavia@gmail.com>
2024-03-05 21:27:18 +0100

    fixed models dependant destroy, avatar seeds, avatar index

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,4 @@
 class Game < ApplicationRecord
   has_many :rounds, dependent: :destroy
   has_many :users, dependent: :destroy
-  has_many :teams, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   belongs_to :game
   belongs_to :team
   has_many :turns
-  has_many :cards
+  has_many :cards, dependent: :destroy
+
   has_one_attached :photo
 
   validates :username, presence: true, uniqueness: true
