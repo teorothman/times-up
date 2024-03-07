f950caabcfa1dc35fcb5d1552e5b0a70127d08b5
Teodor Rothman <106080782+teorothman@users.noreply.github.com>
2024-03-07 12:05:26 +0100

    added create rounds logic to create game

--- db/migrate/20240307100834_add_details_to_games_statuses.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToGamesStatuses < ActiveRecord::Migration[7.1]
-  def change
-    add_column :games_statuses, :turn_status, :string, default: "player_selected"
-    add_column :games_statuses, :start_time, :datetime
-    add_column :games_statuses, :card_skipped, :boolean, default: false
-    add_column :games_statuses, :current_player, :integer
-  end
-end
