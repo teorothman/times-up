1db0a783ae022c36bd71f8c9a6c7295acc1da433
Leo Viguier <leo.viguier@essec.edu>
2024-03-07 11:43:55 +0100

    change title name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_101625) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_103952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,7 +86,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_101625) do
   end
 
   create_table "rounds", force: :cascade do |t|
-    t.string "title"
+    t.string "round_number"
     t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
