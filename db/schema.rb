e77f0e29ccd6c1e8c7984f9a6e1c03a0ada19c87
Teodor Rothman <106080782+teorothman@users.noreply.github.com>
2024-03-08 13:01:16 +0100

    fixed a lot of nice things. pathing in the turns especially

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_165016) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_08_114556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_165016) do
   end
 
   create_table "games_statuses", force: :cascade do |t|
-    t.string "status", default: "pre-lobby"
+    t.string "status", default: "pre_lobby"
     t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
