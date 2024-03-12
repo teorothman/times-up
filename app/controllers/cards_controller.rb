ecfaede3626ff92007398672393d84f06ecc67c8
Leo Viguier <leo.viguier@essec.edu>
2024-03-12 16:25:16 +0100

    almost finish the cable

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,24 +8,39 @@ class CardsController < ApplicationController
       "The Sphinx", "Leonardo da Vinci", "Marilyn Monroe", "Titanic", "Pikachu", "Statue of Liberty", "Michael Jackson", "Sherlock Holmes", "The Colosseum", "Elvis Presley", "Star Wars", "Napoleon Bonaparte", "Spider-Man", "The Hobbit", "Big Ben",
       "The Godfather", "Machu Picchu", "Julius Caesar", "Batman", "Stonehenge", "Jurassic Park", "King Arthur", "Charlie Chaplin", "The Roman Empire", "James Bond", "The Grand Canyon", "Mozart", "Steve Jobs",
     ].sample(3)
-    redirect_to '#' if session[:card_count] > 5
+    redirect_to '#' if current_user.cards.count > 5
   end
 
   def create
     @card = @user.cards.new(card_params)
     @games_status = GamesStatus.find_by(game_id: @game.id)
+    @game = Game.find(params[:game_id])
     @games_status.update(status: "loading")
+
+    @player_order = @game.teams.first.users.to_a.zip(@game.teams.second.users).flatten
+    @player = @player_order[@games_status.turn_counter]
+
     if @card.save
       RoundCard.create(round_id: Game.last.rounds.find_by(round_number: 1).id, card_id: @card.id)
       RoundCard.create(round_id: Game.last.rounds.find_by(round_number: 2).id, card_id: @card.id)
       RoundCard.create(round_id: Game.last.rounds.find_by(round_number: 3).id, card_id: @card.id)
-      session[:card_count] += 1
-      if session[:card_count] <= 5
+      if current_user.cards.count < 5
         redirect_to new_game_user_card_path(@game, @user)
       else
-        session[:card_count] = nil
         if check_all_users_submitted
           @games_status.update(status: "round1_play")
+          GameChannel.broadcast_to(
+            @game,
+            html: render_to_string( partial: "games/player_selected", locals: { game: @game, users: @game.users, game_state: @game_state, player_order: @player_order, rules: @rules, current_user: current_user} ),
+            partial: "player_selected",
+            user: @player.id
+          )
+          PlayerChannel.broadcast_to(
+            @player,
+            # html: render_to_string( partial: "games/player_selected_playing", locals: { game: @game, users: @game.users, game_state: @game_state, player_order: @player_order, rules: @rules, current_user: current_user} ),
+            # partial: "player_selected_playing",
+            message: "hello"
+          )
           redirect_to game_path(@game)
         else
           redirect_to game_path(@game)
@@ -49,8 +64,6 @@ class CardsController < ApplicationController
   def set_game_and_user
     @game = Game.find(params[:game_id])
     @user = User.find(params[:user_id])
-  rescue ActiveRecord::RecordNotFound
-    # Handle cases where Game or User doesn't exist
   end
 
   def check_all_users_submitted
