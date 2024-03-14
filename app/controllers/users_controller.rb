2111edec0f2b7a94ba170fff92c5b06bac8556ac
Teodor Rothman <106080782+teorothman@users.noreply.github.com>
2024-03-14 18:04:25 +0100

    prob right

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
     end
 
     if @user.save
+      session[:user_id] = ""
       session[:user_id] = @user.id
       GameChannel.broadcast_to(
         @game,
